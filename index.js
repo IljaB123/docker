735882da38a496848899103f7ddb54da7dd60a96
IljaB123 <ilja.burzak200p@gmail.com>
2024-10-21 12:48:14 +0000

    Update greeting message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 // Маршрут для динамического приветствия
 app.get('/hello', (req, res) => {
   const name = req.query.name || 'World';  // Получаем параметр name из запроса, по умолчанию "World"
-res.send(`Welcome, ${name}`);
+res.send(`Hi there, ${name}`);
 // Запускаем сервер на порту 3000
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
