fb6b37e42406e33bcdde87507645f6919ad1db75
IljaB123 <ilja.burzak200p@gmail.com>
2024-10-21 11:52:48 +0000

    Changed greeting message to Hello there

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 // Маршрут для динамического приветствия
 app.get('/hello', (req, res) => {
   const name = req.query.name || 'World';  // Получаем параметр name из запроса, по умолчанию "World"
-  res.send(`Hello, ${name}!`);  // Возвращаем сообщение с именем
+  res.send(`Hello there, ${name}!`);  // Возвращаем сообщение с именем
 });
 
 // Запускаем сервер на порту 3000
